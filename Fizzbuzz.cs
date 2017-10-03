8fcd5e002f97a3a1ad2f0f8753481716e1424456
Tara <tburdittx@live.com>
2017-10-03 14:39:22 +0100

    Create app to run FizzBuzz

---
+++ Fizzbuzz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i <= 100; i = i + 1)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+
+                }
+                else if (i % 5 == 0)
+
+                {
+                    Console.WriteLine("buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
